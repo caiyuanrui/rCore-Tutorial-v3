a2a1d3fee9ab5f1dfd35c8e0221e9fb7cdca5314
Cai Yuanrui <caiyuanrui@outlook.com>
2025-04-18 10:10:19 +0800

    Feature: Display bad address and bad instruction when trap happens

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -6,6 +6,7 @@ use core::arch::global_asm;
 
 use riscv::register::{
     scause::{self, Exception, Trap},
+    sstatus::{self, SPP},
     stval, stvec,
     utvec::TrapMode,
 };
@@ -27,12 +28,21 @@ pub fn init() {
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     let scause = scause::read();
     let stval = stval::read();
+
+    if sstatus::read().spp() != SPP::User {
+        panic!("Trap not from user mode")
+    }
+
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
         Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
+            error!(
+                "[kernel] bad address={:#x}, bad instruction={:#x}",
+                stval, cx.sepc
+            );
             error!("[kernel] PageFault in application, kernel killed it");
             run_next_app();
         }
